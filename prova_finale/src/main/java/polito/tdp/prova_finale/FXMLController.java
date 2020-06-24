8c14640cf862bff5d82e942ab36488dd2dd31808
albertodebenedictis <albertodebenedictis@localhost>
2020-06-24 16:50:33 +0200

    Inserita una bozza dell'interfaccia grafica

--- prova_finale/src/main/java/polito/tdp/prova_finale/FXMLController.java
+++ prova_finale/src/main/java/polito/tdp/prova_finale/FXMLController.java
@@ -1,25 +1,63 @@
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
 package polito.tdp.prova_finale;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="choiceQuality"
+    private ChoiceBox<?> choiceQuality; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtIntesa"
+    private TextField txtIntesa; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnTrova"
+    private Button btnTrova; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCosto"
+    private Button btnCosto; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    void doRiduciCosto(ActionEvent event) {
+
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    @FXML
+    void doTrova(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtCampionati(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert choiceQuality != null : "fx:id=\"choiceQuality\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtIntesa != null : "fx:id=\"txtIntesa\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnTrova != null : "fx:id=\"btnTrova\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCosto != null : "fx:id=\"btnCosto\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
 }
