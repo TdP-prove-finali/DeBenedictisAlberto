87873bb1df1d691b505e3387e115bad23cacbb95
albertodebenedictis <s245313@studenti.polito.it>
2020-06-30 14:57:00 +0200

    aggiunto il DB alla cartella

--- prova_finale/src/main/java/polito/tdp/prova_finale/model/Player.java
+++ prova_finale/src/main/java/polito/tdp/prova_finale/model/Player.java
@@ -15,4 +15,83 @@ public class Player {
 	
 	
 	
+	
+	
+	public Player(Integer id, Integer overall, String position, Integer price, String name, String ext_name,
+			String quality, String club, String league, String nationality) {
+		super();
+		this.id = id;
+		this.overall = overall;
+		this.position = position;
+		this.price = price;
+		this.name = name;
+		this.ext_name = ext_name;
+		this.quality = quality;
+		this.club = club;
+		this.league = league;
+		this.nationality = nationality;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getOverall() {
+		return overall;
+	}
+	public void setOverall(Integer overall) {
+		this.overall = overall;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getExt_name() {
+		return ext_name;
+	}
+	public void setExt_name(String ext_name) {
+		this.ext_name = ext_name;
+	}
+	public String getQuality() {
+		return quality;
+	}
+	public void setQuality(String quality) {
+		this.quality = quality;
+	}
+	public String getClub() {
+		return club;
+	}
+	public void setClub(String club) {
+		this.club = club;
+	}
+	public String getLeague() {
+		return league;
+	}
+	public void setLeague(String league) {
+		this.league = league;
+	}
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	
+	
+	
 }
