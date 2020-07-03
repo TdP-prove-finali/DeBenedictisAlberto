e877ffb77307100afed9f22d6e9c4a7a2803df32
albertodebenedictis <s245313@studenti.polito.it>
2020-07-04 00:09:11 +0200

    Terminato il Model
    Terminata la funzione ricorsiva. che sembra avere qualche problema
    (causa NullPointerException perchè chiama troppo presto una funzione che
    richiede tutti i valori compilati)

--- prova_finale/src/main/java/polito/tdp/prova_finale/model/Model.java
+++ prova_finale/src/main/java/polito/tdp/prova_finale/model/Model.java
@@ -3,8 +3,10 @@ package polito.tdp.prova_finale.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 import org.jgrapht.Graph;
@@ -21,15 +23,19 @@ public class Model {
 	Map<Integer, Ruolo> ruoliIdMap;
 
 	Map<String, List<Player>> mappaRuoli;
-	
-	
-	
+
+	List<List<Player>> listaListe;
+
+	boolean trovata;
+	List<TeamPlayer> soluzione;
+
 	public Model() {
 		dao = new PlayersDAO();
 
 	}
 
-	public void creaGrafo(String formation, Integer intesa, Integer maxLeagues, String quality, Integer overall) {
+	public List<TeamPlayer> creaGrafo(String formation, Integer intesa, Integer maxLeagues, String quality,
+			Integer overall) {
 
 		// carico la mappa dei ruoli
 		ruoliIdMap = new HashMap<>();
@@ -81,6 +87,7 @@ public class Model {
 		List<TeamPlayer> vertici = new ArrayList<>(this.grafo.vertexSet());
 		Collections.sort(vertici);
 
+		listaListe = new ArrayList<>();
 		// Preparo una mappa contenente i giocatori che posso mettere
 		// in ogni ruolo in base ai vincoli dell'utente
 		mappaRuoli = new TreeMap<String, List<Player>>();
@@ -89,38 +96,214 @@ public class Model {
 
 			String nomeRuolo = p.getRuolo().getName();
 
-			
-			if(!mappaRuoli.containsKey(nomeRuolo)){
-			
-			List<Player> giocatoriRuolo = new ArrayList<>();
-			
-			giocatoriRuolo = this.dao.getPlayersByParameters(overall, quality, nomeRuolo);
+			// se non ho già preso i giocatori (esempio CB, che compare almeno due volte)
+			if (!mappaRuoli.containsKey(nomeRuolo)) {
 
-			if(giocatoriRuolo.size() == 0) {
-				giocatoriRuolo = this.dao.getPlayersByParameters(overall+1, quality, nomeRuolo);
-			}
-			if(giocatoriRuolo.size() == 0) {
-				System.out.println("Non è stato possibile trovare giocatori che rispettino i requisiti nel ruolo: "+nomeRuolo);
-				return;
-			}
-			
-			System.out.println("Lista giocatore per ruolo "+nomeRuolo+"  creata. Contenuto: " + giocatoriRuolo.size());
+				List<Player> giocatoriRuolo = new ArrayList<>();
 
-			for (Player gioc : giocatoriRuolo) {
-				System.out.println(gioc + "\n");
+				giocatoriRuolo = this.dao.getPlayersByParameters(overall, quality, nomeRuolo);
+
+				// PROBLEMI GIOCATORI NON TROVATI
+				if (giocatoriRuolo.size() == 0) {
+					giocatoriRuolo = this.dao.getPlayersByParameters(overall + 1, quality, nomeRuolo);
+				}
+				if (giocatoriRuolo.size() == 0) {
+					System.out.println("Non è stato possibile trovare giocatori che rispettino i requisiti nel ruolo: "
+							+ nomeRuolo);
+					return null;
+				}
+
+				// riempio la mappa che segna le corrispondenze tra ruolo ed elenco giocatori
+				// sarà questo l'elenco di giocatori su cui farò la ricorsione
+				mappaRuoli.put(nomeRuolo, giocatoriRuolo);
 			}
 
-			mappaRuoli.put(nomeRuolo, giocatoriRuolo);
+		}
+
+		// Creo e inizializzo la soluzione parziale
+		List<TeamPlayer> parziale = new ArrayList<>();
+
+		// inizializzo inserendo un portiere:
+		Player partenza = this.mappaRuoli.get("GK").get(0);
+
+		for (TeamPlayer vertice : this.grafo.vertexSet()) {
+			if (vertice.getRuolo().getName().equals("GK")) {
+				vertice.setPlayer(partenza);
+				parziale.add(vertice);
 			}
 		}
 
-		for (List<Player> lista : mappaRuoli.values()) {
+		trovata = false;
 
-			for (Player p : lista) {
-				System.out.println(p + "\n");
+		ricorsione(parziale, intesa, maxLeagues);
+
+		return soluzione;
+
+	}
+
+	/**
+	 * Aggiungo in modo ricorsivo un giocatore dalla soluzione parziale a scelta tra
+	 * quelli contenuti nella mappaRuoli (elenco di giocatori disponibili per ogni
+	 * ruolo). Dopodiche faccio backtracking
+	 * 
+	 * @param parziale
+	 * @param intesa
+	 * @param maxLeagues
+	 */
+	private void ricorsione(List<TeamPlayer> parziale, Integer intesa, Integer maxLeagues) {
+
+		// se ho trovato la soluzione risalgo i livelli della ricorsione
+		if (trovata) {
+			return;
+		}
+
+		// CASO TERMINALE
+		// se ho 11 giocatori
+		if (parziale.size() == 11) {
+
+			// controllo il numero di campionati
+			if (numeroCampionati(parziale) >= maxLeagues) {
+				// controllo sull'intesa della squadra
+				if (getIntesaSquadra() >= intesa) {
+					this.soluzione = new ArrayList<>(parziale);
+					trovata = true;
+				}
+
+			}
+
+			return;
+		}
+
+		// Creazione di soluzioni
+		for (TeamPlayer vicino : Graphs.neighborListOf(this.grafo, parziale.get(parziale.size() - 1))) {
+
+			// se non ho ancora definito il giocatore nel grafo
+			if (vicino.getPlayer() == null) {
+
+				// scorro tra i giocatori possibili
+				for (Player p : mappaRuoli.get(vicino.getRuolo().getName())) {
+
+					// se non ho già ingaggiato il giocatore nella squadra parziale
+					if (!parziale.contains(p)) {
+						// riempio il vertice nullo con il giocatore nel grafo
+						vicino.setPlayer(p);
+						// aggiungo il giocatore alla lista dei giocatori
+						parziale.add(vicino);
+						// aggiungo gli archi
+						for (TeamPlayer vicino2 : Graphs.neighborListOf(this.grafo, vicino)) {
+							if (vicino2.getPlayer() != null) {
+								this.grafo.setEdgeWeight(this.grafo.getEdge(vicino, vicino2),
+										getPeso(vicino.getPlayer(), vicino2.getPlayer()));
+							}
+						}
+						// faccio ricorsione
+						ricorsione(parziale, intesa, maxLeagues);
+						// faccio backtracking sulla lista e sul grafo (rimuovendo il vertice e
+						// rimettendo i pesi precedenti
+						parziale.remove(vicino);
+						// riporto i pesi alla situazione iniziale
+						for (TeamPlayer vicino2 : Graphs.neighborListOf(this.grafo, vicino)) {
+							if (vicino2.getPlayer() != null) {
+								this.grafo.setEdgeWeight(this.grafo.getEdge(vicino, vicino2), -2);
+							}
+						}
+						vicino.setPlayer(null);
+
+					}
+				}
+			}
+
+		}
+
+	}
+
+	public Integer getIntesaSquadra() {
+
+		Integer intesa = 0;
+
+		for (TeamPlayer tp : this.grafo.vertexSet()) {
+
+			intesa += getIntesaGiocatore(tp);
+		}
+
+		return intesa;
+	}
+
+	public Integer getIntesaGiocatore(TeamPlayer tp) {
+
+		Integer pesoTot = 0;
+
+		for (TeamPlayer vicino : Graphs.neighborListOf(this.grafo, tp)) {
+
+			Integer pesoArco = (int) this.grafo.getEdgeWeight(this.grafo.getEdge(tp, vicino));
+			pesoTot += pesoArco;
+		}
+
+		if (pesoTot >= 0) {
+			return 10;
+		} else if (pesoTot == -1) {
+			return 7;
+		} else {
+			return 4;
+		}
+
+	}
+
+	public Integer getPeso(Player p1, Player p2) {
+
+		if (p1.getClub().equals(p2.getClub())
+				|| (p1.getLeague().equals(p2.getLeague()) && p1.getNationality().equals(p2.getNationality()))) {
+			return 1;
+			// se sono dello stesso campionato o della stessa nazionalità, peso 0
+		} else if (p1.getLeague().equals(p2.getLeague()) || p1.getNationality().equals(p2.getNationality())) {
+			return 0;
+			// se non hanno niente in comune, peso -1
+		} else {
+			return -1;
+		}
+
+	}
+
+	public void setPesi() {
+
+		for (DefaultWeightedEdge e : this.grafo.edgeSet()) {
+
+			Player p1 = this.grafo.getEdgeSource(e).getPlayer();
+			Player p2 = this.grafo.getEdgeTarget(e).getPlayer();
+
+			// Ora vedo cosa hanno in comune
+			// se sono dello stesso club, oppure dello stesso campionato e stessa
+			// nazionalità, peso 1
+			if (p1.getClub().equals(p2.getClub())
+					|| (p1.getLeague().equals(p2.getLeague()) && p1.getNationality().equals(p2.getNationality()))) {
+				this.grafo.setEdgeWeight(e, 1);
+				// se sono dello stesso campionato o della stessa nazionalità, peso 0
+			} else if (p1.getLeague().equals(p2.getLeague()) || p1.getNationality().equals(p2.getNationality())) {
+				this.grafo.setEdgeWeight(e, 0);
+				// se non hanno niente in comune, peso 0
+			} else {
+				this.grafo.setEdgeWeight(e, 0);
 			}
 		}
 
+	}
+
+	/**
+	 * Conta il numero di campionati diversi presente in una lista di giocatori
+	 * 
+	 * @param parziale
+	 * @return
+	 */
+	public int numeroCampionati(List<TeamPlayer> parziale) {
+
+		Set<String> campionati = new HashSet<>();
+
+		for (TeamPlayer p : parziale) {
+
+			campionati.add(p.getPlayer().getLeague());
+		}
+
+		return campionati.size();
 	}
 
 	/**
--- prova_finale/src/main/java/polito/tdp/prova_finale/model/TestModel.java
+++ prova_finale/src/main/java/polito/tdp/prova_finale/model/TestModel.java
@@ -1,12 +1,14 @@
 package polito.tdp.prova_finale.model;
 
+import java.util.List;
+
 public class TestModel {
 
 	public static void main(String[] args) {
 
 		Model model = new Model();
 
-		model.creaGrafo("4-3-3", 90, 4, "Gold - Rare", 80);
+		List<TeamPlayer> soluzione = model.creaGrafo("4-3-3", 60, 3, "Gold - Non-Rare", 76);
 		
 		
 	}
